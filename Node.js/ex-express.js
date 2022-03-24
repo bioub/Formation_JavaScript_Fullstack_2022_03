a0e9e1b5ed6b3a8d218527ce75153b46b6f2ad9c
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2022-03-24 14:19:17 +0100

    Correction Express

--- Node.js/ex-express.js
+++ Node.js/ex-express.js
@@ -1,3 +1,10 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
 const todos = [
   {
     id: 1,
@@ -24,3 +31,62 @@ renvoie en JSON la todo créé avec son id (status code 201)
 - DELETE /todos/:id
 supprime et renvoie la todo supprimée en JSON
 */
+
+
+const app = express();
+
+app.use(cors());
+app.use(morgan('dev'));
+
+
+app.get('/todos', (req, res) => {
+  res.json(todos);
+});
+
+app.post('/todos', express.json(), (req, res) => {
+  const lastTodo = todos.at(-1);
+
+  const nextId = lastTodo ? lastTodo.id + 1 : 1;
+
+  const todo = req.body;
+  todo.id = nextId;
+
+  todos.push(todo);
+
+  res.statusCode = 201;
+  res.json(todo);
+});
+
+app.get('/todos/:id', (req, res) => {
+  const id = +req.params.id; // + converti en Number
+
+  const todo = todos.find((t) => t.id === id);
+
+  if (!todo) {
+    res.statusCode = 404;
+    return res.json({msg: 'Todo not found'});
+  }
+
+  res.json(todo);
+});
+
+app.delete('/todos/:id', (req, res) => {
+  const id = +req.params.id; // + converti en Number
+
+  const todo = todos.find((t) => t.id === id);
+
+  if (!todo) {
+    res.statusCode = 404;
+    return res.json({msg: 'Todo not found'});
+  }
+
+  const index = todos.indexOf(todo);
+  todos.splice(index, 1);
+
+  res.json(todo);
+});
+
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
